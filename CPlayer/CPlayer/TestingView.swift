2d76990e4126306f8accdc27852a19d7452bf846
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-07 18:11:29 +0200

    UnSplash Testing View

--- CPlayer/CPlayer/TestingView.swift
+++ CPlayer/CPlayer/TestingView.swift
@@ -11,10 +11,6 @@ struct TestingView: View {
     var body: some View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            GIFView(gifName: "equal")
-                .frame(width: 70, height: 50)
-                .background(Color.red)
-            
         }
     }
 }
